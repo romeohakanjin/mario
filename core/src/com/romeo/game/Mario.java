1210ff0bfbc02ff129284ae53d1bef969b7dcf4c
Roméo Hakanjin <rhakanjin@Newsco.lan>
2017-01-26 13:45:47 +0100

    Ajout de la map et de la gestion du click utilisateur

--- core/src/com/romeo/game/Mario.java
+++ core/src/com/romeo/game/Mario.java
@@ -11,7 +11,7 @@ import com.romeo.game.Screens.PlayScreen;
 public class Mario extends Game {
 	public SpriteBatch batch;
 	public static final int V_WIDTH = 400;
-	public static final int V_HEIGHT = 300;
+	public static final int V_HEIGHT = 208;
 
 	@Override
 	public void create () {
--- core/src/com/romeo/game/Screens/PlayScreen.java
+++ core/src/com/romeo/game/Screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -22,11 +25,21 @@ public class PlayScreen implements Screen {
     private Viewport gameport;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public PlayScreen(Mario game){
         this.game = game;
         gamecam = new OrthographicCamera();
         gameport = new FitViewport(Mario.V_WIDTH, Mario.V_HEIGHT, gamecam);
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        gamecam.position.set(gameport.getWorldWidth()/2, gameport.getWorldHeight()/2, 0);
     }
 
     @Override
@@ -34,10 +47,27 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched()){
+            gamecam.position.x += 100 * dt;
+        }
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
